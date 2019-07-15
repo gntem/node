b7b25306ae1bad547c17d5642f0a02719934e4b8
Giorgos Ntemiris <ntemirisgiorgos3@gmail.com>
2019-07-15 13:48:47 +0200

    update expectation

--- test/es-module/test-esm-json-translate-error.mjs
+++ test/es-module/test-esm-json-translate-error.mjs
@@ -1,10 +1,14 @@
 // Flags: --experimental-modules
 import { expectsError, mustCall } from '../common/index.mjs';
+import { resolve } from 'path';
 import assert from 'assert';
 
-import('../fixtures/es-modules/translate-invalid-jsonfile.json')
+const pathToFile = resolve(process.cwd(), 'test/fixtures/es-modules/translate-invalid-jsonfile.json');
+
+import(pathToFile)
 .then(assert.fail, expectsError({
-  code: 'ERR_ESM_TRANSLATE_CJS_JSON',
-  reason: 'Unexpected token \n in JSON at position 17',
+    code: 'ERR_ESM_TRANSLATE_CJS_JSON',
+    reason: 'Unexpected token \n in JSON at position 17',
+    pathname: pathToFile,
 }))
 .then(mustCall());
