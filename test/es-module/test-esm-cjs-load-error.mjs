e2861835e7a6e8dab2ff7539254c7bbb3098de0d
Giorgos Ntemiris <ntemirisgiorgos3@gmail.com>
2019-07-24 09:02:51 +0200

    write test import error output

--- test/es-module/test-esm-cjs-load-error.mjs
+++ test/es-module/test-esm-cjs-load-error.mjs
@@ -8,8 +8,13 @@ const entry = fixtures.path('/es-modules/invalid.cjs');
 
 import(entry)
 .then(assert.fail, expectsError({
-    code: 'fire',
-    reason: '',
-    message: 't',
-    pathname: entry,
+    code: 'ESM_CJS_LOAD_ERROR',
+    message: `${entry}:1
+import "invalid";
+       ^^^^^^^^^
+Warning: Attempted to load commonjs module but found static import statement
+Further information can be found at https://nodejs.org/api/esm.html
+
+SyntaxError: Unexpected string
+`,
 }))
