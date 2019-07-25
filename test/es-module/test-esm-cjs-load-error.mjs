ddf122d57aabceafe6963b2bd7413a3581caac49
Giorgos Ntemiris <ntemirisgiorgos3@gmail.com>
2019-07-25 20:16:48 +0200

    applied feedback

--- test/es-module/test-esm-cjs-load-error.mjs
+++ test/es-module/test-esm-cjs-load-error.mjs
@@ -8,13 +8,23 @@ const entry = fixtures.path('/es-modules/invalid.cjs');
 
 import(entry)
 .then(assert.fail, expectsError({
-    code: 'ESM_CJS_LOAD_ERROR',
-    message: `${entry}:1
+    message: `Unexpected string`,
+    stack: `
+Note: To load an ES module set "type": "module" in the package.json or use the .mjs extension.
+
+${entry}:1
 import "invalid";
        ^^^^^^^^^
-Warning: Attempted to load commonjs module but found static import statement
-Further information can be found at https://nodejs.org/api/esm.html
 
 SyntaxError: Unexpected string
-`,
+    at wrapSafe (internal/modules/cjs/loader.js:723:20)
+    at Module._compile (internal/modules/cjs/loader.js:764:27)
+    at Object.Module._extensions..js (internal/modules/cjs/loader.js:805:10)
+    at Module.load (internal/modules/cjs/loader.js:657:32)
+    at Function.Module._load (internal/modules/cjs/loader.js:565:12)
+    at internal/modules/esm/translators.js:86:15
+    at Object.meta.done (internal/modules/esm/create_dynamic_module.js:48:9)
+    at file://${entry}:9:13
+    at ModuleJob.run (internal/modules/esm/module_job.js:111:37)
+    at async Loader.import (internal/modules/esm/loader.js:134:24)`
 }))
